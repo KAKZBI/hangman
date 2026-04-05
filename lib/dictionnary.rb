1926e681200e6d7bbd795472d6cc4bfb56b4fd5f
KAKZBI <enelibrice@yahoo.fr>
2026-04-05 13:07:22 +0100

    Created and implemented dictionary module

--- lib/dictionnary.rb
+++ lib/dictionnary.rb
@@ -1,10 +1,5 @@
-class Dictionary
-  def initialize(file_path)
-    @file_path = file_path
-  end
-
+module Dictionary
   def selectable_words
-    # readlines gives us an array, strip removes newlines, select filters by length
     File.readlines(@file_path).map(&:strip).select { |word| word.length.between?(5, 12) }
   end
 
